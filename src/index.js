fb1d3ed113ec71acc9761be40cfd07e5d88aba2f
Umair Zubair <134955307+umairkodv@users.noreply.github.com>
2026-09-22 20:50:06 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ dotenv.config({
     path: './.env'
 })
 
+
 connectDB().then(() => {
     app.listen(process.env.PORT || 8080, () => {
         console.log(`⚙️ Server is running http://localhost:${process.env.PORT}`);
